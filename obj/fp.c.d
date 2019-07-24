81ad8d3e71c50693e591fbf64972a1bd1d423506
fig02 <fig@DESKTOP-U3G4375.localdomain>
2019-07-23 21:51:31 -0400

    fix makefile for font

--- obj/fp.c.d
+++
@@ -1,7 +0,0 @@
-obj/fp.c.o: src/fp.c src/fp.h src/pm64.h src/gfx.h
-
-src/fp.h:
-
-src/pm64.h:
-
-src/gfx.h:
--- obj/gfx.c.d
+++
@@ -1,5 +0,0 @@
-obj/gfx.c.o: src/gfx.c src/gfx.h src/pm64.h
-
-src/gfx.h:
-
-src/pm64.h:
