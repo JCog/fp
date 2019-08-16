737005fa8aab3b76d385258c7e27da8df321b44f
fig02 <fig02srl@gmail.com>
2019-08-15 20:43:41 -0400

    sync

--- src/fp.h
+++ src/fp.h
@@ -8,13 +8,12 @@
 
 typedef struct  {
     _Bool                   ready;    
-    /*struct vector           watches;*/
+    struct vector           watches;
     size_t                  watch_cnt;
     uint16_t                cheats;
     struct menu             main_menu;
     /*struct settings        *settings;*/
     _Bool                   menu_active;
-    size_t test;
     
 } fp_ctxt_t;
 
