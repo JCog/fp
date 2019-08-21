30804412c8544f975bb8f1a21a9b4036a1658716
fig02 <fig02srl@gmail.com>
2019-08-21 15:31:40 -0400

    getting ready for gfx/menu refactor

--- src/warps_menu.c
+++ src/warps_menu.c
@@ -13,15 +13,8 @@ struct menu *create_warps_menu(){
     static struct menu warps;
     menu_init(&warps,fp.main_menu.x,fp.main_menu.y);
 
-    int x = 0;
-    int y = 0;
-    char group[16] = {0};
-
-
-
     warps.selected_item = menu_add_button(&warps,x,y++,"return",menu_return,NULL);
     menu_add_gray(&warps, x, y++, "current group: ");
-    menu_add_gray(&warps, 10, y, group);
 
     return &warps;
 }
