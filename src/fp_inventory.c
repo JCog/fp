b6a0d45cda89afa2cd6be7ff13dd224180e23584
JCog <jordancogan4@gmail.com>
2021-01-09 05:34:37 -0500

    add labels to items and key items

--- src/fp_inventory.c
+++ src/fp_inventory.c
@@ -1,11 +1,436 @@
 #include "menu.h"
-#include "settings.h"
 #include <stdio.h>
+#include "settings.h"
+#include "gfx.h"
 
 const char *PARTNERS = "none\0""goombario\0""kooper\0""bombette\0"
 "parakarry\0""goompa\0""watt\0""sushie\0""lakilester\0""bow\0""goombaria\0"
 "twink\0";
 const char *RANK = "none\0""super\0""ultra\0";
+const char *ITEM_LIST[] = {
+        "Jump",
+        "Spin Jump",
+        "Tornado Jump",
+        "Hammer",
+        "Super Hammer",
+        "Ultra Hammer",
+        "Lucky Star",
+        "Map",
+        "Big Map",
+        "1st Degree Card",
+        "2nd Degree Card",
+        "3rd Degree Card",
+        "4th Degree Card",
+        "Diploma",
+        "Ultra Stone",
+        "Fortress Key",
+        "Ruins Key",
+        "Pulse Stone",
+        "Castle Key 1",
+        "Palace Key",
+        "Lunar Stone",
+        "Pyramid Stone",
+        "Diamond Stone",
+        "Golden Vase",
+        "Koopers Shell",
+        "Castle Key 2",
+        "Forest Pass",
+        "Weight",
+        "Boos Portrait",
+        "Crystal Berry",
+        "Mystical Key",
+        "Storeroom Key",
+        "Toy Train",
+        "Record",
+        "Frying Pan",
+        "Dictionary",
+        "Mystery Note",
+        "Suspicious Note",
+        "Crystal Ball",
+        "Screwdriver",
+        "Cookbook",
+        "Jade Raven",
+        "Magical Seed 1",
+        "Magical Seed 2",
+        "Magical Seed 3",
+        "Magical Seed 4",
+        "Toad Doll",
+        "Calculator",
+        "Bucket",
+        "Scarf",
+        "Red Key",
+        "Blue Key",
+        "a",
+        "Letter 01",
+        "Letter 02",
+        "Letter 03",
+        "Letter 04",
+        "Letter 05",
+        "Letter 06",
+        "Letter 07",
+        "Letter 08",
+        "Letter 09",
+        "a",
+        "Letter 10",
+        "Letter 11",
+        "a",
+        "a",
+        "a",
+        "Letter 12",
+        "Letter 13",
+        "Letter 14",
+        "Letter 15",
+        "Letter 16",
+        "Letter 17",
+        "Letter 18",
+        "Letter 19",
+        "a",
+        "Letter 20",
+        "Letter 21",
+        "Letter 22",
+        "Letter 23",
+        "Letter 24",
+        "Artifact",
+        "Letter 25",
+        "a",
+        "Dolly",
+        "Water Stone",
+        "Magical Bean",
+        "Fertile Soil",
+        "Miracle Water",
+        "Volcano Vase",
+        "Tape",
+        "Sugar",
+        "Salt",
+        "Egg 1",
+        "Cream",
+        "Strawberry",
+        "Butter",
+        "Cleanser",
+        "Water",
+        "Flour",
+        "Milk",
+        "Lyrics",
+        "Melody",
+        "Mailbag",
+        "Castle Key 3",
+        "Odd Key",
+        "Star Stone",
+        "Sneaky Parasol",
+        "Koopa Legends",
+        "Autograph 1",
+        "Empty Wallet",
+        "Autograph 2",
+        "Koopa Shell",
+        "Old Photo",
+        "Glasses",
+        "Photograph",
+        "Package",
+        "Red Jar",
+        "Castle Key 4",
+        "Warehouse Key",
+        "Prison Key 1",
+        "Silver Credit",
+        "Gold Credit",
+        "Prison Key 2",
+        "Prison Key 3",
+        "Prison Key 4",
+        "Fire Flower",
+        "Snowman Doll",
+        "Thunder Rage",
+        "Shooting Star",
+        "Thunder Bolt",
+        "Pebble",
+        "Dusty Hammer",
+        "Insecticide Herb",
+        "Stone Cap",
+        "Tasty Tonic",
+        "Mushroom",
+        "Volt Shroom",
+        "Super Shroom",
+        "Dried Shroom",
+        "Ultra Shroom",
+        "Sleepy Sheep",
+        "POW Block",
+        "Hustle Drink",
+        "Stop Watch",
+        "Whackas Bump",
+        "Apple",
+        "Life Shroom",
+        "Mystery",
+        "Repel Gel",
+        "Fright Jar",
+        "Please Come Back",
+        "Dizzy Dial",
+        "Super Soda",
+        "Lemon",
+        "Lime",
+        "Blue Berry",
+        "Red Berry",
+        "Yellow Berry",
+        "Bubble Berry",
+        "Jammin Jelly",
+        "Maple Syrup",
+        "Honey Syrup",
+        "Goomnut",
+        "Koopa Leaf",
+        "Dried Pasta",
+        "Dried Fruit",
+        "Strange Leaf",
+        "Cake Mix",
+        "Egg 2",
+        "Coconut",
+        "Melon",
+        "Stinky Herb",
+        "Iced Potato",
+        "Spicy Soup",
+        "Apple Pie",
+        "Honey Ultra",
+        "Maple Ultra",
+        "Jelly Ultra",
+        "Koopasta",
+        "Fried Shroom",
+        "Shroom Cake",
+        "Shroom Steak",
+        "Hot Shroom",
+        "Sweet Shroom",
+        "Yummy Meal",
+        "Healthy Juice",
+        "Bland Meal",
+        "Deluxe Feast",
+        "Special Shake",
+        "Big Cookie",
+        "Cake",
+        "Mistake",
+        "Koopa Tea",
+        "Honey Super",
+        "Maple Super",
+        "Jelly Super",
+        "Spaghetti",
+        "Egg Missile",
+        "Fried Egg",
+        "Honey Shroom",
+        "Honey Candy",
+        "Electro Pop",
+        "Fire Pop",
+        "Lime Candy",
+        "Coco Pop",
+        "Lemon Candy",
+        "Jelly Pop",
+        "Strange Cake",
+        "Kooky Cookie",
+        "Frozen Fries",
+        "Potato Salad",
+        "Nutty Cake",
+        "Maple Shroom",
+        "Boiled Egg",
+        "Yoshi Cookie",
+        "Jelly Shroom 1",
+        "Jelly Shroom 2",
+        "Jelly Shroom 3",
+        "Jelly Shroom 4",
+        "Jelly Shroom 5",
+        "Jelly Shroom 6",
+        "Spin Smash",
+        "Multibounce",
+        "Power Plus A",
+        "Dodge Master",
+        "Power Bounce",
+        "Spike Shield",
+        "First Attack",
+        "HP Plus A",
+        "Quake Hammer",
+        "Double Dip",
+        "Mystery Scroll",
+        "Sleep Stomp",
+        "Fire Shield",
+        "Quick Change",
+        "D Down Pound",
+        "Dizzy Stomp",
+        "Smash Charge 0",
+        "Pretty Lucky",
+        "Feeling Fine",
+        "Attack FXA",
+        "All or Nothing",
+        "HP Drain",
+        "Jump Charge 0",
+        "Slow Go",
+        "FP Plus A",
+        "Mega Rush",
+        "Ice Power",
+        "Defend Plus A",
+        "Pay Off",
+        "Money Money",
+        "Chill Out",
+        "Happy Heart A",
+        "Zap Tap",
+        "Berserker",
+        "Right On",
+        "Runaway Pay",
+        "Refund",
+        "Flower Saver A",
+        "Triple Dip",
+        "Hammer Throw",
+        "Mega Quake",
+        "Smash Charge",
+        "Jump Charge",
+        "S Smash Chg",
+        "S Jump Chg",
+        "Power Rush",
+        "Auto Jump",
+        "Auto Smash",
+        "Crazy Heart",
+        "Last Stand",
+        "Close Call",
+        "P Up D Down",
+        "Lucky Day",
+        "Mega HP Drain",
+        "P Down D Up",
+        "Power Quake",
+        "Auto Multibounce",
+        "Flower Fanatic",
+        "Heart Finder",
+        "Flower Finder",
+        "Spin Attack",
+        "Dizzy Attack",
+        "I Spy",
+        "Speedy Spin",
+        "Bump Attack",
+        "Power Jump",
+        "Super Jump",
+        "Mega Jump",
+        "Power Smash 1",
+        "Super Smash",
+        "Mega Smash",
+        "Power Smash 2",
+        "Power Smash 3",
+        "Deep Focus 1",
+        "Super Focus",
+        "Shrink Smash",
+        "Shell Crack",
+        "Kaiden",
+        "D Down Jump",
+        "Shrink Stomp",
+        "Damage Dodge A",
+        "Earthquake Jump",
+        "Deep Focus 2",
+        "Deep Focus 3",
+        "HP Plus B",
+        "FP Plus B",
+        "Happy Heart B",
+        "Happy Heart X",
+        "Flower Saver B",
+        "Flower Saver X",
+        "Damage Dodge B",
+        "Damage Dodge X",
+        "Power Plus B",
+        "Power Plus X",
+        "Defend Plus X",
+        "Defend Plus Y",
+        "Happy Flower A",
+        "Happy Flower B",
+        "Happy Flower X",
+        "Group Focus",
+        "Peekaboo",
+        "Attack FXD",
+        "Attack FXB",
+        "Attack FXE",
+        "Attack FXC",
+        "Attack FXF",
+        "HP Plus C",
+        "HP Plus X",
+        "HP Plus Y",
+        "FP Plus C",
+        "FP Plus X",
+        "FP Plus Y",
+        "Healthy Healthy",
+        "Attack FXF 2",
+        "Attack FXF 3",
+        "Attack FXF 4",
+        "Attack FXF 5",
+        "Partner Attack",
+        "Heart",
+        "Coin",
+        "Heart Piece",
+        "Star Point",
+        "Full Heal",
+        "Flower",
+        "Star Piece",
+        "Present",
+        "Complete Cake",
+        "Bare Cake",
+        "Empty Cake Pan",
+        "Full Cake Pan",
+        "Empty Mixing Bowl",
+        "Full Mixing Bowl",
+        "Cake With Icing",
+        "Cake With Berries",
+        "Hammer 1 Icon",
+        "Hammer 2 Icon",
+        "Hammer 3 Icon",
+        "Boots 1 Icon",
+        "Boots 2 Icon",
+        "Boots 3 Icon",
+        "Items Icon"
+};
+
+static int item_draw_proc(struct menu_item *item, struct menu_draw_params *draw_params) {
+    gfx_mode_set(GFX_MODE_COLOR, GPACK_RGB24A8(draw_params->color,
+                                               draw_params->alpha));
+    struct gfx_font *font = draw_params->font;
+    int chHeight = menu_get_cell_height(item->owner, 1);
+    int x = draw_params->x;
+    int y = draw_params->y;
+
+    int menuY = 0;
+    int i;
+    for (i = 0; i < 10; i++) {
+        const char *item_string;
+        if (pm_player.items[i] == 0) {
+            item_string = "-";
+        }
+        else {
+            item_string = ITEM_LIST[pm_player.items[i] - 1];
+        }
+        gfx_printf(font, x, y + chHeight * menuY++, "%s", item_string);
+    }
+    return 1;
+}
+
+static int key_item_draw_proc(struct menu_item *item, struct menu_draw_params *draw_params) {
+    gfx_mode_set(GFX_MODE_COLOR, GPACK_RGB24A8(draw_params->color,
+                                               draw_params->alpha));
+    struct gfx_font *font = draw_params->font;
+    int chHeight = menu_get_cell_height(item->owner, 1);
+    int chWidth = menu_get_cell_width(item->owner, 1);
+    int x = draw_params->x;
+    int y = draw_params->y;
+
+    int menuY = 0;
+    int i;
+    for (i = 0; i < 16; i++) {
+        const char *item_string;
+        if (pm_player.key_items[i] == 0) {
+            item_string = "-";
+        }
+        else {
+            item_string = ITEM_LIST[pm_player.key_items[i] - 1];
+        }
+        gfx_printf(font, x, y + chHeight * menuY++, "%.10s", item_string);
+    }
+    menuY = 0;
+    for (; i < 32; i++) {
+        const char *item_string;
+        if (pm_player.key_items[i] == 0) {
+            item_string = "-";
+        }
+        else {
+            item_string = ITEM_LIST[pm_player.key_items[i] - 1];
+        }
+        gfx_printf(font, x + chWidth * 18, y + chHeight * menuY++, "%.10s", item_string);
+    }
+    return 1;
+}
 
 static int halfword_mod_proc(struct menu_item *item,
                              enum menu_callback_reason reason,
@@ -278,9 +703,12 @@ struct menu *create_inventory_menu(void)
     /*build items menu*/
     const int ITEMS_X_0 = 0;
     const int ITEMS_X_1 = 3;
+    const int ITEMS_X_2 = 7;
     y_value = 0;
 
     items.selector = menu_add_submenu(&items, ITEMS_X_0, y_value++, NULL, "return");
+
+    menu_add_static_custom(&items, ITEMS_X_2, y_value, item_draw_proc, NULL, 0xFFFFFF);
 
     int i;
     for (i = 0; i < 10; i++) {
@@ -292,12 +720,14 @@ struct menu *create_inventory_menu(void)
 
     /*build key items menu*/
     const int KEY_ITEMS_X_0 = 0;
-    const int KEY_ITEMS_X_1 = 4;
-    const int KEY_ITEMS_X_2 = 10;
-    const int KEY_ITEMS_X_3 = 14;
+    const int KEY_ITEMS_X_1 = 3;
+    const int KEY_ITEMS_X_2 = 18;
+    const int KEY_ITEMS_X_3 = 21;
     y_value = 0;
 
     key_items.selector = menu_add_submenu(&key_items, KEY_ITEMS_X_0, y_value++, NULL, "return");
+
+    menu_add_static_custom(&key_items, KEY_ITEMS_X_1 + 4, y_value, key_item_draw_proc, NULL, 0xFFFFFF);
 
     for (i = 0; i < 16; i++) {
         char buffer[4];
