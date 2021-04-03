05be6219dd51b880d95fff139a6f046bff1fbd44
JCog <jordancogan4@gmail.com>
2021-04-02 22:42:06 -0400

    update oot ace trainer asm

--- src/trainer.c
+++ src/trainer.c
@@ -97,7 +97,7 @@ static int ace_draw_proc(struct menu_item *item, struct menu_draw_params *draw_p
     int last_timer = pm_ace_store.last_timer;
     gfx_mode_set(GFX_MODE_COLOR, GPACK_RGBA8888(0xFF, 0xFF, 0xFF, 0xFF));
     gfx_printf(font, x, y + chHeight * 0, "effects: ");
-    if (effect_count == 82) {
+    if (effect_count == 81) {
         gfx_mode_set(GFX_MODE_COLOR, GPACK_RGBA8888(0x00, 0xFF, 0x00, 0xFF));
     }
     gfx_printf(font, x + chWidth * 14, y + chHeight * 0, "%d", effect_count);
