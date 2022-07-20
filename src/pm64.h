e1919d33bffb160a0d912cfb1f5ed29e45d60c2b
JCog <jordancogan4@gmail.com>
2022-07-20 16:38:25 -0500

    move vec typedefs to types.h

--- src/pm64.h
+++ src/pm64.h
@@ -26,41 +26,6 @@
 #define ICONS_BP_ROM_START           0x13C820
 #endif
 
-typedef union {
-    struct {
-        f32 f_odd;
-        f32 f_even;
-    } f;
-    f64 d;
-} __OSfp;
-
-typedef struct {
-    /* 0x00 */ s8 x;
-    /* 0x01 */ s8 y;
-} vec2b_t; // size = 0x02
-
-typedef struct {
-    /* 0x00 */ u8 x;
-    /* 0x01 */ u8 y;
-} vec2bu_t; // size = 0x02
-
-typedef struct {
-    /* 0x00 */ f32 x;
-    /* 0x04 */ f32 z;
-} vec2xzf_t; // size = 0x08
-
-typedef struct {
-    /* 0x00 */ f32 x;
-    /* 0x04 */ f32 y;
-    /* 0x08 */ f32 z;
-} vec3f_t; // size = 0x0C
-
-typedef struct {
-    /* 0x00 */ s16 x;
-    /* 0x02 */ s16 y;
-    /* 0x04 */ s16 z;
-} vec3s_t; // size = 0x06
-
 typedef struct {
     /* 0x00 */ s8 xCardinal;
     /* 0x01 */ s8 yCardinal;
--- src/types.h
+++ src/types.h
@@ -14,4 +14,31 @@ typedef float f32;
 typedef double f64;
 typedef _Bool bool;
 
+typedef struct {
+    /* 0x00 */ s8 x;
+    /* 0x01 */ s8 y;
+} vec2b_t; // size = 0x02
+
+typedef struct {
+    /* 0x00 */ u8 x;
+    /* 0x01 */ u8 y;
+} vec2bu_t; // size = 0x02
+
+typedef struct {
+    /* 0x00 */ f32 x;
+    /* 0x04 */ f32 z;
+} vec2xzf_t; // size = 0x08
+
+typedef struct {
+    /* 0x00 */ f32 x;
+    /* 0x04 */ f32 y;
+    /* 0x08 */ f32 z;
+} vec3f_t; // size = 0x0C
+
+typedef struct {
+    /* 0x00 */ s16 x;
+    /* 0x02 */ s16 y;
+    /* 0x04 */ s16 z;
+} vec3s_t; // size = 0x06
+
 #endif //_TYPES_H_
