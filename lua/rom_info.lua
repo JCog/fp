63cf3a42588f83845cad3abd92597185759c1ddf
imaandrew <14946103+imaandrew@users.noreply.github.com>
2022-11-22 18:09:06 -0500

    Update rom build scripts

--- lua/rom_info.lua
+++ lua/rom_info.lua
@@ -4,7 +4,7 @@ roms = {
         ldr_rom = 0x25E2C,
         ldr_addr = 0x8004AA2C,
         dma_func = 0x800296FC,
-        rom_id = "JP",
+        rom_id = "jp",
         payload_addr = 0x1C7F220,
     },
     [0xA7F5CD7E] = {
@@ -12,7 +12,7 @@ roms = {
         ldr_rom = 0x2617C,
         ldr_addr = 0x8004AD7C,
         dma_func = 0x8002973C,
-        rom_id = "US",
+        rom_id = "us",
         payload_addr = 0x1C84D30,
     }
 }
