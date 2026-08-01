cf3941bb95499ff2980303d2c77c68f8519a32cb
imaandrew <14946103+imaandrew@users.noreply.github.com>
2026-08-01 14:42:39 -0400

    add new font with more characters

--- src/sys/crash_screen.c
+++ src/sys/crash_screen.c
@@ -12,25 +12,6 @@ typedef struct {
 
 static CrashScreen gCrashScreen;
 
-static const u8 gCrashScreencharToGlyph[128] = {
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, 41, -1, -1, -1, 43, -1, -1, 37, 38, -1, 42, -1, 39, 44, -1, 0,  1,  2,  3,
-    4,  5,  6,  7,  8,  9,  36, -1, -1, -1, -1, 40, -1, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-    23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, -1, -1, -1, -1, -1, -1, 10, 11, 12, 13, 14, 15, 16,
-    17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, -1, -1, -1, -1, -1,
-};
-
-static const u32 gCrashScreenFont[] = {
-    0x70871C30, 0x8988A250, 0x88808290, 0x88831C90, 0x888402F8, 0x88882210, 0x71CF9C10, 0xF9CF9C70,
-    0x8228A288, 0xF200A288, 0x0BC11C78, 0x0A222208, 0x8A222288, 0x71C21C70, 0x23C738F8, 0x5228A480,
-    0x8A282280, 0x8BC822F0, 0xFA282280, 0x8A28A480, 0x8BC738F8, 0xF9C89C08, 0x82288808, 0x82088808,
-    0xF2EF8808, 0x82288888, 0x82288888, 0x81C89C70, 0x8A08A270, 0x920DA288, 0xA20AB288, 0xC20AAA88,
-    0xA208A688, 0x9208A288, 0x8BE8A270, 0xF1CF1CF8, 0x8A28A220, 0x8A28A020, 0xF22F1C20, 0x82AA0220,
-    0x82492220, 0x81A89C20, 0x8A28A288, 0x8A28A288, 0x8A289488, 0x8A2A8850, 0x894A9420, 0x894AA220,
-    0x70852220, 0xF8011000, 0x08020800, 0x10840400, 0x20040470, 0x40840400, 0x80020800, 0xF8011000,
-    0x70800000, 0x88822200, 0x08820400, 0x108F8800, 0x20821000, 0x00022200, 0x20800020, 0x00000000,
-};
-
 static const char *gFaultCauses[18] = {
     "Interrupt",
     "TLB modification",
@@ -90,46 +71,50 @@ static void crashScreenDrawRect(s32 x, s32 y, s32 width, s32 height) {
     }
 }
 
-static void crashScreenDrawGlyph(s32 x, s32 y, s32 glyph) {
-    s32 shift = ((glyph % 5) * 6);
+static void crashScreenDrawGlyph(s32 x, s32 y, char c, u16 fg, u16 bg) {
     u16 width = gCrashScreen.width;
-    const u32 *data = &gCrashScreenFont[glyph / 5 * 7];
+    const u8 *rows;
     s32 i;
     s32 j;
 
+    if (c < CRASH_FONT_GLYPH_MIN || c > CRASH_FONT_GLYPH_MAX) {
+        c = '?';
+    }
+    rows = gCrashScreenFont[c - CRASH_FONT_GLYPH_MIN];
+
     if (width == 320) {
-        u16 *ptr = gCrashScreen.frameBuf + (gCrashScreen.width) * y + x;
+        u16 *ptr = gCrashScreen.frameBuf + width * y + x;
 
-        for (i = 0; i < 7; i++) {
-            u32 bit = 0x80000000U >> shift;
-            u32 rowMask = *data++;
+        for (i = 0; i < CRASH_FONT_HEIGHT; i++) {
+            u8 bit = 0x10;
+            u8 rowMask = *rows++;
 
-            for (j = 0; j < 6; j++) {
-                *ptr++ = (bit & rowMask) ? 0xFFFF : 1;
+            for (j = 0; j < CRASH_FONT_WIDTH; j++) {
+                *ptr++ = (bit & rowMask) ? fg : bg;
                 bit >>= 1;
             }
 
-            ptr += gCrashScreen.width - 6;
+            ptr += width - CRASH_FONT_WIDTH;
         }
     } else if (width == 640) {
-        u16 *ptr = gCrashScreen.frameBuf + (y * 0x500) + (x * 2);
+        u16 *ptr = gCrashScreen.frameBuf + width * (y * 2) + (x * 2);
 
-        for (i = 0; i < 7; i++) {
-            u32 bit = 0x80000000U >> shift;
-            u32 rowMask = *data++;
+        for (i = 0; i < CRASH_FONT_HEIGHT; i++) {
+            u8 rowMask = *rows++;
+            u8 bit = 0x10;
 
-            for (j = 0; j < 6; j++) {
-                u16 temp = (bit & rowMask) ? 0xFFFF : 1;
+            for (j = 0; j < CRASH_FONT_WIDTH; j++) {
+                u16 temp = (bit & rowMask) ? fg : bg;
 
                 ptr[0] = temp;
                 ptr[1] = temp;
-                ptr[640] = temp;
-                ptr[641] = temp;
+                ptr[width] = temp;
+                ptr[width + 1] = temp;
                 ptr += 2;
                 bit >>= 1;
             }
 
-            ptr += (0x9E8 / 2);
+            ptr += (width - CRASH_FONT_WIDTH) * 2;
         }
     }
 }
@@ -141,7 +126,6 @@ static void *crashScreenCopyToBuf(void *dest, const char *src, u32 size) {
 
 static void crashScreenPrintf(s32 x, s32 y, const char *fmt, ...) {
     u8 *ptr;
-    u32 glyph;
     s32 size;
     u8 buf[0x100];
     va_list args;
@@ -154,13 +138,7 @@ static void crashScreenPrintf(s32 x, s32 y, const char *fmt, ...) {
         ptr = buf;
 
         while (size > 0) {
-            const u8 *charToGlyph = gCrashScreencharToGlyph;
-
-            glyph = charToGlyph[*ptr & 0x7F];
-
-            if (glyph != 0xFF) {
-                crashScreenDrawGlyph(x, y, glyph);
-            }
+            crashScreenDrawGlyph(x, y, (char)*ptr, 0xFFFF, 1);
 
             x += 6;
             size--;
